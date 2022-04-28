d640be51b53837ade3c49be746e0cca709f49be7
ritikx01 <mail.ritik01@gmail.com>
2022-04-28 17:37:33 +0530

    Update installer

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ if [ ! -s "resolvers.txt" ]; then
   printf "${cyan}Setup script has run previously. To force execution, delete '.ins_check'.${reset}"
 
 echo "${cyan}This script requires root privillege. Run with sudo"
-echo "Please install Go and Python manually before running this script${reset}"
+echo "Please install Go, Rust and Python manually before running this script${reset}"
 sleep 5
 
 
@@ -17,6 +17,30 @@ mkdir .tmp
 wget "https://raw.githubusercontent.com/ritikx01/figlet_fonts/main/Delta%20Corps%20Priest%201.flf" -O .tmp/dcp1.flf
 mv .tmp/dcp1.flf /usr/share/figlet
 apt install figlet lolcat cowsay
+
+mkdir ~/Toools 2>/dev/nul
+#DNS validator
+git clone https://github.com/vortexau/dnsvalidator.git ~/Tools/dnsvalidator
+cd ~/Tools/dnsvalidator
+python3 setup.py install
+#MassDNS
+git clone https://github.com/blechschmidt/massdns.git ~/Tools/massdns
+cd ~/Tools/massdns
+make 
+sudo cp bin/massdns /usr/local/bin
+#PureDNS
+go install github.com/d3mondev/puredns/v2@latest
+#Subfinder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+# Amass
+go install -v github.com/OWASP/Amass/v3/...@master
+#httpx
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+#gospider
+GO111MODULE=on go get -u github.com/jaeles-project/gospider
+#NtHiM
+cargo install NtHiM
+
 
 #Installation check
 touch .ins_check
